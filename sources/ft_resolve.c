661a602c39b6026e792cb0c584f6da65c5659ee7
Juluka <lnkambul@student.wethinkcode.co.za>
2020-05-16 11:16:53 +0200

    worked out minor bug in ft_resolve

--- sources/ft_resolve.c
+++ sources/ft_resolve.c
@@ -7,21 +7,22 @@ char	*ft_resolve(char *command, char *env[])
 	char	*sub;
 
 	i = -1;
-	if (ft_strchr(command, '/'))
-		sub = ft_strsub(command, 1, ft_strlen(command) -\
-			ft_strlen(ft_strchr(command,'/')));
-	else
-		sub = ft_strsub(command, 1, ft_strlen(command) - 1);
+	sub = NULL;
+	sub = (ft_strchr(command, '/')) ? ft_strsub(command, 1,\
+		ft_strlen(command) - ft_strlen(ft_strchr(command,'/'))) :\
+		 ft_strsub(command, 1, ft_strlen(command) - 1);
+	if (command[0] == '~')
+	{
+		(sub) ? free(sub) : NULL;
+		sub = ft_strdup("HOME");
+	}
 	while (env[++i])
 	{
 		if ((temp = ft_strstr(env[i], sub)))
 		{
-			if (temp[ft_strlen(sub)] == '=')
-			{
-				temp = &(env[i][ft_strlen(sub) + 1]);
-				free(sub);
-				return (temp);
-			}
+			temp = &(env[i][ft_strlen(sub) + 1]);
+			free(sub);
+			return (temp);
 		}
 	}
 	free(sub);
