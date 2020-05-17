250eb8752fcdac1ef741ad455de89532d413b533
Juluka <lnkambul@student.wethinkcode.co.za>
2020-05-17 21:41:42 +0200

    fixed memory leak on symlink cd

--- sources/ft_cd.c
+++ sources/ft_cd.c
@@ -9,10 +9,11 @@ void	ft_cd(char **command[], char **env[])
 	args = *command;
 	if (ft_arrlen(args) > 2)
 		ft_putendl("minishell: cd: too many arguments");
-	else if (!(args[1]) || ft_strcmp(args[1], "~") == 0 ||\
-			ft_strcmp(args[1], "--") == 0)
+	else if ((!(args[1]) || ft_strcmp(args[1], "~") == 0 ||\
+			ft_strcmp(args[1], "--") == 0) && ft_resolve("HOME",
+				*env))
 		path = ft_strdup(ft_resolve("$HOME", *env));
-	else if (ft_strcmp(args[1], "-") == 0)
+	else if (ft_strcmp(args[1], "-") == 0 && ft_resolve("$OLDPWD", *env))
 		path = ft_strdup(ft_resolve("$OLDPWD", *env));
 	else
 		path = ft_dirpath(args[1], env);
--- sources/ft_dirpath.c
+++ sources/ft_dirpath.c
@@ -7,8 +7,7 @@ char	*ft_dirpath(char *args, char **env[])
 	char	link[PATH_MAX];
 
 	temp = NULL;
-	temp = ft_resolve(args, *env);
-	if (temp)
+	if (args[0] != '/' && (temp = ft_resolve(args, *env)))
 		path = (ft_strchr(args, '/')) ? ft_strjoin(temp,\
 			 ft_strchr(args, '/')) : ft_strdup(temp);
 	else
@@ -20,8 +19,9 @@ char	*ft_dirpath(char *args, char **env[])
 	}
 	else if (ft_checkdir(path) == 2)
 	{
-		readlink(args, ft_memset(link, 0, PATH_MAX), PATH_MAX);
+		readlink(path, ft_memset(link, 0, PATH_MAX), PATH_MAX);
 		temp = ft_strsub(path, 0, (ft_strrchr(path, '/') - path + 1));
+		free(path);
 		path = ft_strjoin(temp, link);
 		(temp) ? free(temp) : NULL;
 	}
