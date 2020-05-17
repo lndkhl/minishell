29546df5f1b881b94775b6194ee4b099ca388067
Juluka <lnkambul@student.wethinkcode.co.za>
2020-05-17 20:36:07 +0200

    cd setenv bug fixed, all functions coded, testing left

--- sources/ft_setcd.c
+++ sources/ft_setcd.c
@@ -2,19 +2,18 @@
 
 void	ft_setcd(char *path, char **env[])
 {
+	char	*exec;
 	char	*old;
 	char	*current;
 	char	**command;
 
+	exec = ft_strdup("setenv");
 	old = ft_strjoin("OLDPWD=", ft_resolve("$PWD", *env));
 	current = ft_strjoin("PWD=", path);
-	ft_putstr("old = ");
-	ft_putendl(old);
-	ft_putstr("current = ");
-	ft_putendl(current);
-	command = (char **)malloc(sizeof(char *) * 3);
-	command[0] = old;
-	command[1] = current;
-	command[2] = NULL;
+	command = (char **)malloc(sizeof(char *) * 4);
+	command[0] = exec;
+	command[1] = old;
+	command[2] = current;
+	command[3] = NULL;
 	ft_setenv(&command, env);
 }
